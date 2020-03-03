c03357d0dc63820a4291433d559678b5d92f623e
ditdittdittt <ditdittdittt@gmail.com>
2020-03-04 00:32:42 +0700

    [yudit] Test CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Assalamualaikum
                 </div>
 
                 <div class="links">
