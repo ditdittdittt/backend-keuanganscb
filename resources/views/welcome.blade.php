905336df45abc542a6f502385d271d0c2d4566c5
ditdittdittt <ditdittdittt@gmail.com>
2020-03-04 00:45:14 +0700

    [yudit] Test CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Waalaikumsalam
+                    Assalamualaikum
                 </div>
 
                 <div class="links">
