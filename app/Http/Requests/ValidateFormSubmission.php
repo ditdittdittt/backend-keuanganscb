6d58821fde4caeba7e1e128b4b3bf024db613122
ditdittdittt <ditdittdittt@gmail.com>
2020-03-19 09:50:00 +0700

    [yudit] fix bug in validate form submission

--- app/Http/Requests/ValidateFormSubmission.php
+++ app/Http/Requests/ValidateFormSubmission.php
@@ -24,7 +24,6 @@ class ValidateFormSubmission extends FormRequest
     public function rules()
     {
         return [
-            'user_id' => 'required',
             'date' => 'required',
             'used' => 'required',
             'allocation' => 'required',
